550b0a5e384d1cffcc7c86fd4e80e3da643599a8
kjeongh <wellsy001012@gmail.com>
2025-02-02 04:25:57 +0900

    feat: 이벤트 신청 throttling

--- apps/apply/src/apply.controller.ts
+++ apps/apply/src/apply.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Post } from '@nestjs/common';
 import { ApplyService } from './apply.service';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller('apply')
 export class ApplyController {
@@ -8,6 +9,12 @@ export class ApplyController {
     ) {}
 
     @Post('/event')
+    @Throttle({
+        default: {
+            limit: 3,
+            ttl: 30000,
+        }
+    })
     async apply(@Body() body: any): Promise<string> {
         const userId = body.userId;
         return this.applyService.applyEvent(userId);
--- apps/apply/src/apply.module.ts
+++ apps/apply/src/apply.module.ts
@@ -3,13 +3,22 @@ import { Module } from '@nestjs/common';
 import { ApplyController } from './apply.controller';
 import { ApplyService } from './apply.service';
 import { RedisModule, RedisService } from '@daengro/redis';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
     imports: [
         KafkaModule.forRoot(),
         RedisModule.forRoot(),
+        ThrottlerModule.forRoot([{
+            ttl: 3000,
+            limit: 5,
+        }])
     ],
-    providers: [ApplyService, KafkaService, RedisService],
+    providers: [ApplyService, KafkaService, RedisService, {
+        provide: APP_GUARD,
+        useClass: ThrottlerGuard,
+    }],
     controllers: [ApplyController]
 })
 export class ApplyModule {}
